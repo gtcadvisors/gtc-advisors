1c5272e7a5d89e47fd33a2c18a4ce40a9425549b
Muwaheed <muwaheedmustapha@gmail.com>
2023-01-31 08:34:14 +0100

    add all tax expert files

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'taxexperts';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'password';
+$config['db']['pre'] = 'tx_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '1.0';
+$config['installed'] = '1';
+?>

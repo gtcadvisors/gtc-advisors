5668ac4b4beab679770fc1b9df3fd2a6af4f87c4
Muwaheed <muwaheedmustapha@gmail.com>
2023-02-15 11:25:00 +0100

    recreated advisor profile page

--- templates/classic-theme/header_expert.php
+++ templates/classic-theme/header_expert.php
@@ -17,7 +17,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link href="<?php _esc(TEMPLATE_URL); ?>/assets/css/vendors/line-awesome.min.css">
     <link href="<?php _esc(TEMPLATE_URL); ?>/assets/vendors/slick-master/slick/slick.css" rel="stylesheet" type="text/css">
-    <!--<link href="<?php _esc(TEMPLATE_URL); ?>/assets/css/vendors/style.css" rel="stylesheet" type="text/css">-->
+
+    
     <link href="<?php _esc(TEMPLATE_URL); ?>/assets/css/e.css" rel="stylesheet" type="text/css">
     <title>Expert Profile Agency </title>
   </head>
